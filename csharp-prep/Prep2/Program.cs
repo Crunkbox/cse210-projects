56bae4c809f3f1d8bc268f97e7c7757afcafdf12
Thomas Meyers <funclemoz@gmail.com>
2024-04-25 13:42:37 -0600

    Created a grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("PREPARE TO BE JUDGED. SUBMIT THINE GRADE. ");
+        string grade = Console.ReadLine();
+        string letter = "";
+        if (int.Parse(grade) >= 90)
+        {
+            letter = "A";
+        }
+        else if (int.Parse(grade) >= 80)
+        {
+            letter = "B";
+        }
+        else if (int.Parse(grade) >= 70)
+        {
+            letter = "C";
+        }
+        else if (int.Parse(grade) >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        
+        
+        if (int.Parse(grade) >= 70)
+        {
+            Console.Write($"THINE GRADE IS '{letter}'. THOU DIDST ACHIEVE EXCELLANCE!");
+        }
+        else
+        {
+            Console.Write($"THINE GRADE IS '{letter}'. I SHALL CAST YOU OUT FROM MY PRECENSE!");
+        }
     }
 }
